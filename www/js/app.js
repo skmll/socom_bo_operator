64bd6793fbd83c035839925f2f9d971aa749e431
Rafael Costa <rafael.costa90@gmail.com>
2015-05-08 01:20:06 +0100

    added a simple use of a rest api

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,10 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic']);
 
-.run(function($ionicPlatform) {
+
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -15,5 +16,13 @@ angular.module('starter', ['ionic'])
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
+  }); 
+})
+
+app.controller('OperatorTestCtrl', function($scope, operatorService) {
+  $scope.operators = 'Loading data..';  
+  operatorService.getRandomWord().then(function(data){
+    console.log(data['data']['weather']);
+    $scope.operators = 'London weather: ' + data['data']['weather'][0].description;
   });
 })
--- www/js/services/operator-stub-service.js
+++ www/js/services/operator-stub-service.js
@@ -1,3 +1,9 @@
-.service('OperatorStubService', function() {
-
-});
+app.service('operatorService', ['$http', function ($http) {
+	
+	var baseUrl = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk';
+	
+    this.getRandomWord = function () {
+		return $http.get(baseUrl);
+	};
+	
+}]);
