c43ba06b9a33d2e41febdb9a6f5a9a3c72f7d70a
Rafael Costa <rafael.costa90@gmail.com>
2015-05-18 16:15:57 +0100

    request post simplified

--- www/js/services/master-stub-service.js
+++ www/js/services/master-stub-service.js
@@ -1,19 +1,19 @@
 app.factory('MasterStubService', ['$http', function ($http) {
 	var baseUrl = 'http://192.168.234.37/SOCOM_BO/public/v1/';
 	var data = {};
-
-	
-
-	data.login = function (username, password) {
-		var req = {
+	var requestPost = 
+	{
 		 method: 'POST',
-		 url: baseUrl + 'master/login',
 		 headers: {
 		   'Content-Type': 'application/x-www-form-urlencoded'
 		 },
-		 params: { username: escape(username), password: escape(password) }
-		}
-		return $http(req);
+	};
+	
+
+	data.login = function (username, password) {
+		requestPost.url = baseUrl + 'master/login';
+		requestPost.params = { username: escape(username), password: escape(password) };
+		return $http(requestPost);
 	};
 		
 	return data;
