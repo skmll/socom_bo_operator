1c97f06f11e85143209a94929db544f95143c4ed
WikiDreams <danielsantosvieiramail@gmail.com>
2015-05-20 10:40:18 +0100

    project updated

--- www/js/controllers/commonController.js
+++ www/js/controllers/commonController.js
@@ -1,14 +1,3 @@
 app.controller('CommonController', function($scope, CommonStubService) {
 
-	$scope.getAllEvents = function(){
-		CommonStubService.getAllEvents()
-		.success(function (data) {
-			console.log(data);
-			$scope.events = data;
-		})
-		.error(function (error) {
-			$scope.events = 'Unable to load events: ' + error;
-		});
-	}
-
 });
