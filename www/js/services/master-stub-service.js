0b2e343e67f7a9754b88dea1236bef4c6da0b4cf
Rafael Costa <rafael.costa90@gmail.com>
2015-06-08 15:07:23 +0100

    asd

--- www/js/services/master-stub-service.js
+++ www/js/services/master-stub-service.js
@@ -16,7 +16,7 @@ app.factory('MasterStubService', function ($http) {
 
 	function escapeIfNotNull(variable){
 		if(variable != null){
-			return escape(variable);
+			return variable;
 		}
 		return null;
 	}
