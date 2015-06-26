42f657784faaf404dd194fb4b8dec83fd0be98b8
Rafael Costa <rafael.costa90@gmail.com>
2015-06-26 16:46:12 +0100

    services updated

--- www/js/services/master-stub-service.js
+++ www/js/services/master-stub-service.js
@@ -282,15 +282,15 @@ app.factory('MasterStubService', function ($http) {
 			return $http.get(baseUrl + 'master/zone/type/get/id/' + id);
 		},
 
-		createMasterZone : function (id, description, capture_points) {
+		createMasterZone : function (id, description, capture_points, color) {
 			requestPost.url = baseUrl + 'master/zone/create';
-			requestPost.params = { zone_id: escapeIfNotNull(id), description: escapeIfNotNull(description), capture_points: escapeIfNotNull(capture_points) };
+			requestPost.params = { zone_id: escapeIfNotNull(id), description: escapeIfNotNull(description), capture_points: escapeIfNotNull(capture_points), color: color };
 			return $http(requestPost);
 		},
 
-		updateMasterZone : function (id, description, capture_points) {
+		updateMasterZone : function (id, description, capture_points, color) {
 			requestPost.url = baseUrl + 'master/zone/update/' + id;
-			requestPost.params = { zone_id: escapeIfNotNull(id), description: escapeIfNotNull(description), capture_points: escapeIfNotNull(capture_points) };
+			requestPost.params = { zone_id: escapeIfNotNull(id), description: escapeIfNotNull(description), capture_points: escapeIfNotNull(capture_points), color: color };
 			return $http(requestPost);
 		},
 
